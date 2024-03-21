361ffe1ef2e2bc55b6561d375b90c8807869002f
Jeff Sinclair <jeffs@thought-logic.com>
2024-03-21 12:04:53 -0400

    updating if then

--- staffingdb.py
+++ staffingdb.py
@@ -85,13 +85,15 @@ class ProjectAdd:
             near_shore_spread = 0
         elif (self.nearshore_cb == True and (self.offshore_cb == False | self.timezone == 5 | self.availability ==5)):
             near_shore_spread = (near_shore_score/(on_shore_score + near_shore_score + off_shore_score) ) + (off_shore_score/(on_shore_score + near_shore_score + off_shore_score))
+        elif (self.nearshore_cb == True and self.offshore_cb == False):
+            near_shore_spread = (near_shore_score/(on_shore_score + near_shore_score + off_shore_score) ) + (off_shore_score/(on_shore_score + near_shore_score + off_shore_score))
         else:
             near_shore_spread = near_shore_score/(on_shore_score + near_shore_score + off_shore_score)
         if self.offshore_cb == True:
             if (self.timezone == 5 | self.availability == 5):
                 off_shore_spread = 0
             elif (self.nearshore_cb == True):
-                
+
                 off_shore_spread = off_shore_score/(on_shore_score + near_shore_score + off_shore_score)
             else:
                 off_shore_spread = off_shore_score/(on_shore_score + near_shore_score + off_shore_score) + near_shore_score/(on_shore_score + near_shore_score + off_shore_score)
