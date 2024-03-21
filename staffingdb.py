5cdf2101bba9b2539f0bb05aebb9fa83a8b34c61
Jeff Sinclair <jeffs@thought-logic.com>
2024-03-21 16:58:40 -0400

    test

--- staffingdb.py
+++ staffingdb.py
@@ -83,7 +83,7 @@ class ProjectAdd:
             on_shore_spread = on_shore_score/(on_shore_score + near_shore_score + off_shore_score)
         if self.nearshore_cb == False and self.offshore_cb == False:
             near_shore_spread = 0
-        elif self.timezone ==5 or self.availability ==5:
+        elif self.nearshore_cb == True and (self.timezone ==5 or self.availability ==5):
             near_shore_spread = near_shore_score/(on_shore_score + near_shore_score + off_shore_score) + off_shore_score/(on_shore_score + near_shore_score + off_shore_score)
         elif self.nearshore_cb == True and self.offshore_cb == False:
             near_shore_spread = near_shore_score/(on_shore_score + near_shore_score + off_shore_score) + off_shore_score/(on_shore_score + near_shore_score + off_shore_score)
