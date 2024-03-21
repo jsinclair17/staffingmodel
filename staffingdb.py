6f573e702c3f29b974ef05a4f19a38266324732b
Jeff Sinclair <jeffs@thought-logic.com>
2024-03-21 13:03:03 -0400

    update spread if then

--- staffingdb.py
+++ staffingdb.py
@@ -81,26 +81,13 @@ class ProjectAdd:
             on_shore_spread = 1
         else:
             on_shore_spread = on_shore_score/(on_shore_score + near_shore_score + off_shore_score)
+        
         if self.nearshore_cb == False:
             near_shore_spread = 0
-        elif (self.nearshore_cb == True and (self.offshore_cb == False | self.timezone == 5 | self.availability ==5)):
-            near_shore_spread = (near_shore_score/(on_shore_score + near_shore_score + off_shore_score) ) + (off_shore_score/(on_shore_score + near_shore_score + off_shore_score))
-        elif (self.nearshore_cb == True and self.offshore_cb == True):
-            near_shore_spread = (near_shore_score/(on_shore_score + near_shore_score + off_shore_score) ) + (off_shore_score/(on_shore_score + near_shore_score + off_shore_score))
-        elif (self.nearshore_cb == True and self.offshore_cb == False):
-            near_shore_spread = (near_shore_score/(on_shore_score + near_shore_score + off_shore_score) ) + (off_shore_score/(on_shore_score + near_shore_score + off_shore_score))
+        elif self.nearshore_cb == True and (self.offshore_cb == False | self.timezone ==5 | self.availability ==5):
+            near_shore_spread = near_shore_score/(on_shore_score + near_shore_score + off_shore_score) + off_shore_score/(on_shore_score + near_shore_score + off_shore_score)
         else:
             near_shore_spread = near_shore_score/(on_shore_score + near_shore_score + off_shore_score)
-        if self.offshore_cb == True:
-            if (self.timezone == 5 | self.availability == 5):
-                off_shore_spread = 0
-            elif (self.nearshore_cb == True):
-
-                off_shore_spread = off_shore_score/(on_shore_score + near_shore_score + off_shore_score)
-            else:
-                off_shore_spread = off_shore_score/(on_shore_score + near_shore_score + off_shore_score) + near_shore_score/(on_shore_score + near_shore_score + off_shore_score)
-        else:
-            off_shore_spread = 0
         low_savings_calc = '{:.0%}'.format((1-(((on_shore_cost*on_shore_spread) + (off_shore_cost*off_shore_spread) + (near_shore_cost*near_shore_spread) )/ on_shore_cost))-0.05)
         high_savings_calc = '{:.0%}'.format((1-(((on_shore_cost*on_shore_spread) + (off_shore_cost*off_shore_spread) + (near_shore_cost*near_shore_spread) )/ on_shore_cost))+0.05) 
         return [[('On Shore',on_shore_spread), ('Off Shore',off_shore_spread) , ('Near Shore', near_shore_spread)],(low_savings_calc, high_savings_calc)]
