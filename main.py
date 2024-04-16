b77264a7e485ba49a930c5cfe560fe0d6551f2f7
Jeff Sinclair <jeffs@thought-logic.com>
2024-04-15 21:59:34 -0400

    pushing old code

--- main.py
+++ main.py
@@ -27,12 +27,11 @@ st.image(my_logo)
 
 
 customer_name = st.text_input("Customer Name", key='customer')
-customer_address = st.text_input("Customer Email Address", key='address')
+#customer_address = st.text_input("Customer Email Address", key='address')
 
 project_name = st.text_input("Project Name", key='projectname')
 project_type = st.selectbox("Project Type", ["Data Engineering","Data Migration","Pipelines and Intergration","Data Story Telling"
 ,"Application Development","Testing/ Functional UAT"], key='projecttype') 
-project_desc = st.text_input("Project Description", key='projectdesc')
 
 #st.divider()
 with st.sidebar:
@@ -42,33 +41,7 @@ with st.sidebar:
     nearshore_cb = col1.checkbox('Near Shore', value=True, help='Our near shore team are fluent English speakers located in Bogota, Columbia')
     offshore_cb = col2.checkbox('Off Shore', value=True, help='Our off shore team are fluent English speakers located in Pakistan')
 st.sidebar.title('Choose Project Priorities')
-project_area_names_complexity = st.sidebar.slider("Go To Market Strategy", min_value=1, max_value=5, help='Describe the level of planning needed to deliver the product' ,key='complex')
-with st.sidebar:
-    if project_area_names_complexity ==1:
-        st.sidebar.write('Proof of Concept')
-    elif project_area_names_complexity ==2:
-        st.write('Protoytpe Development')
-    elif project_area_names_complexity ==3: 
-        st.write('MVP')
-    elif project_area_names_complexity ==4:
-        st.write('Scaling Strategy Development')
-    else:
-        st.write('Full Feature Launch')
-#    st.divider()
-project_area_names_innovate = st.sidebar.slider("Innovation & Process Maturity", min_value=1, max_value=5, help='Describe the need to be more Strategic or Tactical', key='innovate')
-with st.sidebar:
-    if project_area_names_innovate ==1:
-        st.write('Routine')
-    elif project_area_names_innovate ==2:
-        st.write('Flexible Process')
-    elif project_area_names_innovate ==3:
-        st.write('Strategic Innovation')
-    elif project_area_names_innovate ==4:
-        st.write('Transformational Innovation')
-    else:
-        st.write('Disruptor')
-#st.divider()
-#project_area_names_cost = st.sidebar.slider("Savings", min_value=1, max_value=5, help='Your enagagment funding level has been allocated', key='cost')
+project_area_names_cost = st.sidebar.slider("Savings", min_value=1, max_value=5, help='Your enagagment funding level has been allocated', key='cost')
 # if project_area_names_cost <3:
 #     st.sidebar.write('Conservative Spend')
 # elif project_area_names_cost >=3 and project_area_names_cost <5:
@@ -80,72 +53,87 @@ with st.sidebar:
 # else:
 #     st.sidebar.write('Large Investment Made')
 #st.divider()
-project_area_names_speed = st.sidebar.slider("Time To Deliver", min_value=1, max_value=5, help='Describe the amount of capacity to deliver the product', key='speed')
-with st.sidebar:
-    if project_area_names_speed ==1:
-        st.sidebar.write('4-6 Weeks')
-    elif project_area_names_speed ==2:
-        st.write('6-12 Weeks')
-    elif project_area_names_speed ==3: 
-        st.write('12-20 Weeks')
-    elif project_area_names_speed ==4:
-        st.write('20-30 Weeks')
-    else:
-        st.write('30+ Weeks')
-#    st.divider()
-project_area_names_expertise = st.sidebar.slider("Expertise Required", min_value=1, max_value=5, help='Skillset necessary for successful delivery of the initiative', key='expert')
-with st.sidebar:
-    if project_area_names_expertise <3:
-        st.sidebar.write('Functional Expert')
-    elif project_area_names_expertise >=3 and project_area_names_expertise <5:
-        st.write('Skilled Practictioner')
-    else:
-        st.write('Subject Matter Experts')
-project_area_names_laws = st.sidebar.slider("Laws & Regulations", min_value=1, max_value=5, help='Describe the barriers to using offshore due to the nature of the work', key='laws')
-with st.sidebar:
-    if project_area_names_laws ==1:
-        st.write('Lenient')
-    elif project_area_names_laws ==2:
-        st.write('Moderate Oversight')
-    elif project_area_names_laws ==3:
-        st.write('Enhanced Oversight')
-    elif project_area_names_laws ==4:
-        st.write('Highly Regulated')
-    else:
-        st.write('Restrictive')
-# st.divider()
-project_area_names_accessibility = st.sidebar.slider("Response Time", min_value=1, max_value=5, help='Communication promptness required', key="accessible")
-with st.sidebar:
-    if project_area_names_accessibility ==1:
-        st.write('Within 1-2 Business Days')
-    elif project_area_names_accessibility ==2:
-        st.write('Within 6-8 hours during Operating Hours')
-    elif project_area_names_accessibility ==3:
-        st.write('Within 4-6 hours during Operating Hours')
-    elif project_area_names_accessibility ==4:
-        st.write('Within 2-4 hours during Operating Hours')
-    else:
-        st.write('Fully accessible during Operating Hours')
+project_area_names_speed = st.sidebar.slider("Speed", min_value=1, max_value=5, help='Describe the amount of capacity to deliver the product', key='speed')
+# if project_area_names_speed <3:
+#     st.write('Proof of Concept')
+# elif project_area_names_speed >=3 and project_area_names_speed <5:
+#     st.write('Protoytpe Development')
+# elif project_area_names_speed >=5 and project_area_names_speed <7:
+#     st.write('MVP')
+# elif project_area_names_speed >=7 and project_area_names_speed <10:
+#     st.write('Scaling Strategy Development')
+# else:
+#     st.write('Full Feature Launch')
 #st.divider()
 #project_area_names_criticallity = st.slider("Criticallity (Org Impact)", min_value=1, max_value=10, key='critical')
-project_area_names_tz = st.sidebar.slider("Timezone Coordination", min_value=1, max_value=5, help='Describe the need for resources to be time zone aligned' ,key='timezone')
-with st.sidebar:
-    if project_area_names_tz ==1:
-        st.write('Minimal Overlap Required')
-    elif project_area_names_tz ==2:
-        st.write('Overlap as requested')
-    elif project_area_names_tz ==3:
-        st.write('Adaptive Scheduling Required')
-    elif project_area_names_tz ==4:
-        st.write('Alignment Required for Most Deliverables')
-    else:
-        st.write('Required')
+project_area_names_tz = st.sidebar.slider("Timezone Critical", min_value=1, max_value=5, help='Describe the need for resources to be time zone aligned' ,key='timezone')
+# if project_area_names_tz <3:
+#     st.write('Minimal Overlap Required')
+# elif project_area_names_tz >=3 and project_area_names_tz <5:
+#     st.write('Overlap as requested')
+# elif project_area_names_tz >=5 and project_area_names_tz <7:
+#     st.write('Adaptive Scheduling Required')
+# elif project_area_names_tz >=7 and project_area_names_tz <10:
+#     st.write('Alignment Required for Most Deliverables')
+# else:
+#     st.write('Required')
 #st.divider()
-
-
-
-
-
+project_area_names_complexity = st.sidebar.slider("Go To Market Strategy", min_value=1, max_value=5, help='Describe the level of planning needed to deliver the product' ,key='complex')
+# if project_area_names_complexity <3:
+#     st.write('Individual Contributor')
+# elif project_area_names_complexity >=3 and project_area_names_complexity <5:
+#     st.write('Individual Contributor with Advisory Support')
+# elif project_area_names_complexity >=5 and project_area_names_complexity <7:
+#     st.write('Functional Project Team')
+# elif project_area_names_complexity >=7 and project_area_names_complexity <10:
+#     st.write('Functional Project Team with Advisory Support')
+# else:
+#     st.write('Large Program Team')
+#st.divider()
+project_area_names_expertise = st.sidebar.slider("Skill Level", min_value=1, max_value=5, help='Skillset necessary for successful delivery of the initiative', key='expert')
+# if project_area_names_expertise <5:
+#     st.write('Functional Expert')
+# elif project_area_names_expertise >=5 and project_area_names_expertise <10:
+#     st.write('Skilled Practicioner')
+# else:
+#     st.write('Subject Matter Expert')
+#st.divider()
+project_area_names_laws = st.sidebar.slider("Team Size", min_value=1, max_value=5, help='Describe the level of capacity needed to support the initiative', key='laws')
+# if project_area_names_laws <3:
+#     st.write('Lenient')
+# elif project_area_names_laws >=3 and project_area_names_laws <5:
+#     st.write('Moderate Oversight')
+# elif project_area_names_laws >=5 and project_area_names_laws <7:
+#     st.write('Enhanced Oversight')
+# elif project_area_names_laws >=7 and project_area_names_laws <10:
+#     st.write('Highly Regulated')
+# else:
+#     st.write('Restrictive')
+#st.divider()
+project_area_names_accessibility = st.sidebar.slider("Response Time", min_value=1, max_value=5, help='Communication promptness required', key="accessible")
+# if project_area_names_accessibility <3:
+#     st.write('Within 1-2 Business Days')
+# elif project_area_names_accessibility >=3 and project_area_names_accessibility <5:
+#     st.write('Within 6-8 hours during Operating Hours')
+# elif project_area_names_accessibility >=5 and project_area_names_accessibility <7:
+#     st.write('Within 4-6 hours during Operating Hours')
+# elif project_area_names_accessibility >=7 and project_area_names_accessibility <10:
+#     st.write('Within 2-4 hours during Operating Hours')
+# else:
+#     st.write('Fully accessible during Operating Hours')
+#st.divider()
+project_area_names_innovate = st.sidebar.slider("Innovation & Process Maturity", min_value=1, max_value=5, help='Describe the need to be more Strategic or Tactical', key='innovate')
+# if project_area_names_innovate <3:
+#     st.write('Routine')
+# elif project_area_names_innovate >=3 and project_area_names_innovate <5:
+#     st.write('Flexible Process')
+# elif project_area_names_innovate >=5 and project_area_names_innovate <7:
+#     st.write('Strategic Innovation')
+# elif project_area_names_innovate >=7 and project_area_names_innovate <10:
+#     st.write('Transformational Innovation')
+# else:
+#     st.write('Disruptor')
+#st.divider()
 #project_area_names_scalabilty = st.slider("Scalability", min_value=1, max_value=10, key='scalable')
 #staff_name = st.text_input("Staff Name")
 #staff_type_name = st.selectbox("Staff Type", ["Engineer", "Manager", "Designer"])
@@ -157,22 +145,13 @@ with st.sidebar:
 customer1 = stdb.ProjectAdd(
     customer_name
     #, customer_address
-    , project_name
-    , project_type
-    # , 
-    # project_area_names_cost
-    , project_area_names_complexity
-    , project_area_names_innovate
-    , project_area_names_speed
-    , project_area_names_expertise
-    , project_area_names_laws
-    , project_area_names_accessibility
-    , project_area_names_tz
-    #, project_area_names_criticallity
-
-
-    #, project_area_names_scalabilty
-    ,nearshore_cb, offshore_cb
+    , project_name, project_type, 
+    project_area_names_cost, project_area_names_speed
+                            #, project_area_names_criticallity
+                            , project_area_names_tz, project_area_names_complexity, project_area_names_expertise, project_area_names_laws, project_area_names_accessibility
+                            , project_area_names_innovate
+                            #, project_area_names_scalabilty
+                            ,nearshore_cb, offshore_cb
                             )
 #customer1.save()
 spread_val = customer1.getspread()
@@ -182,7 +161,6 @@ def set_value():
     st.session_state['address'] = ''
     st.session_state['projectname'] = ''
     st.session_state['projecttype'] = 'Data Engineering'
-    st.session_state['projectdesc'] = ''
     st.session_state['cost'] = 1
     st.session_state['speed'] = 1
     #st.session_state['critical'] = 1
